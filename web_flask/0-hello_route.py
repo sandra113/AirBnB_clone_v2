030222bac7e7144366dd1efcd83c88a2bfbbae7d
root <sandramabonga303@gmail.com>
2023-12-30 14:26:24 -0800

    start flask

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -3,13 +3,16 @@
 A script that starts a flask web application
 """
 
-from flask import Flask
 
+from flask import Flask
 app = Flask(__name__)
+
 
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
+    """return Hello HBNB!"""
     return 'Hello HBNB!'
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
